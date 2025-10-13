d39574ddb51351c753de41b35f7ff6205aa1c09c
Simón Dinamarca <simondinamarca86@gmail.com>
2025-10-13 00:08:51 -0300

    proyecto completo

---
+++ redbarrio/.angular/cache/20.3.3/app/vite/deps/md.transition-D8TeJ_Pz-TBA5A2SK.js
@@ -0,0 +1,12 @@
+import {
+  mdTransitionAnimation
+} from "./chunk-UTUNQVJQ.js";
+import "./chunk-KGCUZFT7.js";
+import "./chunk-NTNOC4F3.js";
+import "./chunk-2QSFQOVL.js";
+import "./chunk-LCMILTBF.js";
+import "./chunk-ALXP5TSD.js";
+import "./chunk-QXFS4N4X.js";
+export {
+  mdTransitionAnimation
+};
