d8c366193bf6e2a284696e9b5a8eae19db5740df
DOOMHAK <ra.nunezp@duocuc.cl>
2025-10-27 23:20:58 -0300

    transparencia
    transparenciaa

---
+++ RedBarrioApp-bck1/.angular/cache/20.3.3/app/vite/deps/@jcesarmobile_capacitor-ocr.js
@@ -0,0 +1,11 @@
+import {
+  registerPlugin
+} from "./chunk-F6BL72UZ.js";
+import "./chunk-QXFS4N4X.js";
+
+// node_modules/@jcesarmobile/capacitor-ocr/dist/esm/index.js
+var Ocr = registerPlugin("Ocr");
+export {
+  Ocr
+};
+//# sourceMappingURL=@jcesarmobile_capacitor-ocr.js.map
---
+++ RedBarrioApp-bck1/.angular/cache/20.3.3/app/vite/deps/ios.transition-eAEkgVAv-J4IDRRFN.js
@@ -0,0 +1,14 @@
+import {
+  iosTransitionAnimation,
+  shadow
+} from "./chunk-QREBLPPY.js";
+import "./chunk-PQ6JLT2J.js";
+import "./chunk-SLZTZ6YS.js";
+import "./chunk-T4VX5F6C.js";
+import "./chunk-LCMILTBF.js";
+import "./chunk-34I4FW2K.js";
+import "./chunk-QXFS4N4X.js";
+export {
+  iosTransitionAnimation,
+  shadow
+};
